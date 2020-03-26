79d3f94978019438e22a103559f831b80d9fa21f
Hazumi Yuuri <hazumi.yuuri@gmail.com>
2020-03-26 11:42:18 +0300

    Adding recipes modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,17 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingComponent } from './shopping/shopping.component';
 import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ShoppingComponent,
-    RecipesComponent
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent
   ],
   imports: [
     BrowserModule,
